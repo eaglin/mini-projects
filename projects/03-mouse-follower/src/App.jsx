65ae8e856c661106f87e4ac7a21754b627b38f12
mike <miguel.vazquez@laude.tech>
2023-06-25 00:08:12 +0200

    mouse y nuevo project

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,53 @@
+
+import './App.css'
+import { useEffect, useState } from 'react'
+function App() {
+
+  const [enabled, setEnable] = useState(false);
+  const [position, setPosition] = useState({ clientX: 0, clientY: 0 });
+  useEffect(() => {
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ clientX, clientY })
+
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+
+    return () => { window.removeEventListener('pointermove', handleMove) }
+  }, [enabled])
+
+
+
+
+
+  return (
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.clientX}px,${position.clientY}px)`
+      }}></div>
+      <h3>
+        Proyecto 3
+      </h3>
+      <button onClick={() => setEnable(!enabled)}>
+        {enabled ? "Activado" : "Desactivado"}
+      </button>
+
+
+    </main>
+  )
+}
+
+export default App
